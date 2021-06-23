6e3d0b22e6e6d16f6ae134b740417a205ebc8c67
Eb3r-r00t <eberson_taynan@hotmail.com>
2021-06-23 01:00:13 -0300

    Alteração da função de seleção item presisonado

--- GUI/Menus/MenuBar/MenuButtonArq.gd
+++ GUI/Menus/MenuBar/MenuButtonArq.gd
@@ -8,18 +8,9 @@ extends 'res://GUI/Menus/MenuBar/MenuButtonMain.gd'
 func _ready():
 	for i in ["File","Save","Quit"]:
 		get_popup().add_item(i)
+		
 	get_popup().set_item_shortcut(0,set_shortcut(KEY_F),true)
 	get_popup().set_item_shortcut(1,set_shortcut(KEY_S),true)
-	get_popup().set_item_shortcut(2,set_shortcut(KEY_Q),true)
-	
+	get_popup().set_item_shortcut(2,set_shortcut(KEY_Q),true)	
 	get_popup().connect("id_pressed", self, "_on_item_pressed")
 	
-func _on_item_pressed(id):
-	var item_name = get_popup().get_item_text(id)
-	if item_name == "Quit":
-		get_tree().quit()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- GUI/Menus/MenuBar/MenuButtonMain.gd
+++ GUI/Menus/MenuBar/MenuButtonMain.gd
@@ -10,7 +10,13 @@ extends MenuButton
 func _ready():
 	pass # Replace with function body.
 
+#Function to pick up item pressed
+func _on_item_pressed(id):
+	var item_name = get_popup().get_item_text(id)
+	if item_name == "Quit":
+		get_tree().quit()
 
+#Function to insert shortcut on buttons 
 func set_shortcut(key):
 	var shortcut = ShortCut.new()
 	var inputeventkey = InputEventKey.new()
