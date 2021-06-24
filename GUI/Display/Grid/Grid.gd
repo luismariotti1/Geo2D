b3f87afdf25471d36490d525ed65b2ebff80ade7
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-06-24 11:33:15 -0300

    static numbers

--- GUI/Display/Grid/Grid.gd
+++ GUI/Display/Grid/Grid.gd
@@ -6,6 +6,8 @@ var clicked_position = null
 var is_clicking = false
 var distX = 0
 var distY = 0
+var label = Label.new()
+var font = label.get_font("")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -37,24 +39,28 @@ func sub_axis(subdivision = 24):
 		shiftX -= subdivision_distance
 		if shiftX > rect_position.x and shiftX < rect_size.x:
 			draw_line(Vector2(shiftX,rect_position.y), Vector2(shiftX,rect_size.y),sublines_color, 1.0, false)
+			draw_string (font, Vector2(shiftX,axis_center_position.y+15),str("-",1), color, -1)
 	
 	shiftX = axis_center_position.x
 	while shiftX < rect_size.x:
 		shiftX += subdivision_distance
 		if shiftX < rect_size.x and shiftX > rect_position.x:
 			draw_line(Vector2(shiftX,rect_position.y), Vector2(shiftX,rect_size.y),sublines_color, 1.0, false)
+			draw_string (font, Vector2(shiftX,axis_center_position.y+15),str(1), color, -1)
 
 	var shiftY = axis_center_position.y
 	while shiftY > rect_position.y:
 		shiftY -= subdivision_distance
 		if shiftY > rect_position.y and shiftY < rect_size.y:
 			draw_line(Vector2(rect_position.x,shiftY), Vector2(rect_size.x,shiftY),sublines_color, 1.0, false)
+			draw_string (font, Vector2(axis_center_position.x+5,shiftY),str("-",1), color, -1)
 
 	shiftY = axis_center_position.y
 	while shiftY < rect_size.y:
 		shiftY += subdivision_distance
 		if shiftY < rect_size.y and shiftY > rect_position.y:
 			draw_line(Vector2(rect_position.x,shiftY), Vector2(rect_size.x,shiftY),sublines_color, 1.0, false)
+			draw_string (font, Vector2(axis_center_position.x+5,shiftY),str("-",1), color, -1)
 
 func create_axis_x():
 	draw_line(Vector2(rect_position.x,axis_center_position.y), Vector2(rect_size.x,axis_center_position.y), color, 1.25, false)
@@ -76,4 +82,6 @@ func _draw():
 	if cand_draw_y():
 		create_axis_y()
 	sub_axis()
+#   Talvez uma possivel forma de manter a resolução
+#	print(get_viewport().size, get_viewport().get_final_transform ( ))
 	
