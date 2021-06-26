65d67320a83779e54777af5846cd59fbace08676
Diego <dih.cvel@gmail.com>
2021-06-26 17:06:12 -0300

    disabled button incmpleto

--- GUI/Menus/SideMenu/Panel/Panel.gd
+++ GUI/Menus/SideMenu/Panel/Panel.gd
@@ -1,5 +1,7 @@
 extends Control
 
+
+
 var img = [
 	"res://assets/Icons/Buttons/Triangle/buttonTriangulo.png",
 	"res://assets/Icons/Buttons/Triangle/buttonTrianguloPressed.png",
@@ -17,9 +19,27 @@ onready var spr1 =$quadradoButton/Sprite
 onready var spr2 =$HexagonoButton/Sprite
 onready var spr3 =$verticeButton/Sprite
 
+func disabled_button():
+	pass
+
+func _ready():
+	for x in ['t', 'q', 'h', 'v']:
+		if x == $trianguloButton.get_text():
+			$trianguloButton.set_disabled(false)
+		elif x == $quadradoButton.get_text():
+			$quadradoButton.set_disabled(false)
+		elif x == $HexagonoButton.get_text():
+			$HexagonoButton.set_disabled(false)
+		elif x == $verticeButton.get_text():
+			$verticeButton.set_disabled(false)
+	pass
+
 func swap_button(button, a, b):
 	if click == InputEventMouseButton.CONNECT_DEFERRED:
 		button.texture = load(img[a])
+#		button1.visible = !$Sprite.visible
+#		button3.visible = !$Sprite.visible
+#		button3.visible = !$Sprite.visible
 		click = 0
 	else:
 		button.texture = load(img[b])
@@ -34,7 +54,7 @@ func mouse_entered_exited(button, a):
 
 # config botao triangulo
 func _on_trianguloButton_mouse_entered():
-	$trianguloButton.set_tooltip("Triangle")
+	$trianguloButton.set_tooltip("Triangle Button")
 	mouse_entered_exited(spr0, 0.7)
 	pass # Replace with function body.
 
@@ -50,7 +70,7 @@ func _on_trianguloButton_pressed():
 
 # config botao quadrado
 func _on_quadradoButton_mouse_entered():
-	$quadradoButton.set_tooltip("Square")
+	$quadradoButton.set_tooltip("Square Button")
 	mouse_entered_exited(spr1, 0.7)
 	pass # Replace with function body.
 
@@ -60,13 +80,14 @@ func _on_quadradoButton_mouse_exited():
 
 func _on_quadradoButton_pressed():
 	swap_button(spr1, 2, 3)
+	
 	pass # Replace with function body.
 
 
 
 # config botao hexagona
 func _on_HexagonoButton_mouse_entered():
-	$HexagonoButton.set_tooltip("Hexagon")
+	$HexagonoButton.set_tooltip("Hexagon Button")
 	mouse_entered_exited(spr2, 0.7)
 	pass # Replace with function body.
 
@@ -82,7 +103,7 @@ func _on_HexagonoButton_pressed():
 
 # config botao vertice
 func _on_verticeButton_mouse_entered():
-	$verticeButton.set_tooltip("Vertex")
+	$verticeButton.set_tooltip("Vertex Button")
 	mouse_entered_exited(spr3, 0.7)
 	pass # Replace with function body.
 
