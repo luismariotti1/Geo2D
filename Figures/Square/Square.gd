be866446d0cef1f5ae5f56138a589c3ef657b96e
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-06-28 17:13:50 -0300

    primeiras transformações

--- Figures/Square/Square.gd
+++ Figures/Square/Square.gd
@@ -1,13 +1,31 @@
 extends Control
 
 onready var CP = get_node("/root/CartesianPlane")
-var l = 5;
-var h = 2;
+var l = 1;
 var line_width = 2.0;
 var color = ColorN("red")
+var transform : Transform2D
+var pivot : Vector2
+var rot = 0.5
+var scaled 
+
+func _ready():
+	transform = Transform2D.IDENTITY
+	pivot = Vector2(0,0)
 
 func _physics_process(delta):
+#	rot += 0.5 * delta
+#	var t = Transform2D(Vector2(1,0),Vector2(0,1),Vector2(0,0))
+#	t.x.x = cos(rot)
+#	t.y.y = cos(rot)
+#	t.x.y = sin(rot)
+#	t.y.x = -sin(rot)
+	transform = Transform2D.IDENTITY.scaled(Vector2(2,2))
+#	transform = t
+	transform.origin = CP.convert_cartesian_to_pos(Vector2(1,1))
+	print(transform)
 	update()
 
 func _draw():
-	draw_rect (Rect2(CP.get_center_plane(),CP.convert_catersian_to_dist(Vector2(l,h))),color, false, 2.0, false )
+	draw_set_transform_matrix (transform)
+	draw_rect (Rect2(Vector2(0,0),CP.convert_catersian_to_dist(Vector2(l,l))),color, false, 2.0, false )
--- GlobalAttributes/CartesianPlane.gd
+++ GlobalAttributes/CartesianPlane.gd
@@ -14,5 +14,8 @@ func set_center_plane(center):
 func get_center_plane():
 	return center_plan
 
+func convert_cartesian_to_pos(coord):
+	return Vector2(center_plan.x + coord.x*cartesian_distance, center_plan.y + -1*coord.y*cartesian_distance)	
+
 func convert_catersian_to_dist(coord):
 	return Vector2(coord.x*cartesian_distance,-1*coord.y*cartesian_distance)
