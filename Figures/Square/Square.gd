b905df4ede55c750ad4d9e0f61d5a22dc6fc762a
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-06-28 18:29:49 -0300

    file do goias

--- Figures/Square/Square.gd
+++ Figures/Square/Square.gd
@@ -2,30 +2,31 @@ extends Control
 
 onready var CP = get_node("/root/CartesianPlane")
 var l = 1;
-var line_width = 2.0;
+var line_width = 2.0
 var color = ColorN("red")
-var transform : Transform2D
-var pivot : Vector2
-var rot = 0.5
-var scaled 
+var t : Transform2D
+var rotate = false
 
 func _ready():
-	transform = Transform2D.IDENTITY
-	pivot = Vector2(0,0)
+	t = Transform2D(Vector2(1,0),Vector2(0,1),Vector2(0,0))
 
 func _physics_process(delta):
 #	rot += 0.5 * delta
-#	var t = Transform2D(Vector2(1,0),Vector2(0,1),Vector2(0,0))
+	if rotate == false:
+		t = t.rotated(deg2rad(180))
+		print(t)
+		rotate = true
 #	t.x.x = cos(rot)
 #	t.y.y = cos(rot)
 #	t.x.y = sin(rot)
 #	t.y.x = -sin(rot)
-	transform = Transform2D.IDENTITY.scaled(Vector2(2,2))
-#	transform = t
-	transform.origin = CP.convert_cartesian_to_pos(Vector2(1,1))
-	print(transform)
+#	t.x *= 2
+#	t.y *= 2
+	t.origin = CP.convert_cartesian_to_pos(Vector2(0,0))
+#	print(t)
 	update()
 
 func _draw():
-	draw_set_transform_matrix (transform)
-	draw_rect (Rect2(Vector2(0,0),CP.convert_catersian_to_dist(Vector2(l,l))),color, false, 2.0, false )
+	draw_set_transform_matrix(t)
+	draw_rect(Rect2(Vector2(0,0),CP.convert_catersian_to_dist(Vector2(l,l))),color, false, 2.0, false)
+
