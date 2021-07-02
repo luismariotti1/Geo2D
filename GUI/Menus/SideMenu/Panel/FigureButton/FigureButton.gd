ddc707b2d52d58171c0ab9d56e0829b5adba40fa
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-07-02 20:10:58 -0300

    up

--- GUI/Menus/SideMenu/Panel/FigureButton/FigureButton.gd
+++ GUI/Menus/SideMenu/Panel/FigureButton/FigureButton.gd
@@ -1,18 +1,25 @@
 extends Button
 
 var _imgs = []
-#var can_be_pressed = true
-#var pressed = false
+var _id : int
 
-func init(imgs):
+func init(id, imgs):
 	_imgs = imgs
 	set_image(_imgs[0])
-	
+	_id = id
+
+
+func get_id():
+	return _id
+
+
 func set_image(img):
 	get_node("Sprite").texture = load(img)
+
 
 func _on_FigureButton_toggled(button_pressed):
 	if button_pressed and !get_parent().has_one_clicked:
 		set_image(_imgs[1])
 	else:
+		get_parent().button_clicked = -1
 		set_image(_imgs[0])
