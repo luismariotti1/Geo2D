5561d48a1a1db33aa5803ef9cac312353d99a3ba
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-07-02 20:04:25 -0300

    FUNCIONOU PORRA

--- GUI/Menus/SideMenu/Panel/FigureButton/FigureButton.gd
+++ GUI/Menus/SideMenu/Panel/FigureButton/FigureButton.gd
@@ -1,18 +1,26 @@
 extends Button
 
 var _imgs = []
-#var can_be_pressed = true
-#var pressed = false
+var _id: int
 
-func init(imgs):
+
+func init(id, imgs):
+	_id = id
 	_imgs = imgs
 	set_image(_imgs[0])
-	
+
+
+func get_id():
+	return _id
+
+
 func set_image(img):
 	get_node("Sprite").texture = load(img)
 
+
 func _on_FigureButton_toggled(button_pressed):
-	if button_pressed and !get_parent().has_one_clicked:
+	if button_pressed:
 		set_image(_imgs[1])
 	else:
+		get_parent().button_clicked = -1
 		set_image(_imgs[0])
--- GUI/Menus/SideMenu/Panel/ShowButtons.gd
+++ GUI/Menus/SideMenu/Panel/ShowButtons.gd
@@ -9,24 +9,28 @@ var imgs = [
 	"res://assets/Icons/Buttons/Hexagon/buttonHexagonoPressed.png",
 	"res://assets/Icons/Buttons/Vertex/buttonVertice.png",
 	"res://assets/Icons/Buttons/Vertex/buttonVerticePressed.png"
-	]
-	
+]
+
 var button = load("res://GUI/Menus/SideMenu/Panel/FigureButton/FigureButton.tscn")
 var buttons = []
-var has_one_clicked = false
-# var enable = buttons.set_disabled(true)
+var button_clicked = -1
+
 
 func _ready():
-	for i in range(4):
-		buttons.append(button.instance()) 
-		buttons[buttons.size() - 1].init([imgs[i*2], imgs[i*2+1]])
+	for i in range(imgs.size() / 2):
+		buttons.append(button.instance())
+		buttons[buttons.size() - 1].init(i, [imgs[i * 2], imgs[i * 2 + 1]])
 		add_child(buttons[buttons.size() - 1])
 
-#func _process(delta):
-#	has_one_clicked = false
-#	for i in range(buttons.size()):
-#		# buttons[i].disabled = true
-#		if buttons[i].pressed:
-#			has_one_clicked = true
-#		buttons[i].disabled = true	
 
+func _process(delta):
+	for i in len(buttons):
+		if button_clicked == -1:
+			buttons[i].disabled = false
+	for i in len(buttons):
+		if buttons[i].pressed:
+			button_clicked = buttons[i].get_id()
+	for i in len(buttons):
+		if button_clicked != -1:
+			if buttons[i].get_id() != button_clicked:
+				buttons[i].disabled = true
