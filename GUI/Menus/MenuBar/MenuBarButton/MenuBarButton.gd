18c5dd1b64d45940b0fbe91f0754a6550a9ed6b4
Eb3r-r00t <eberson_taynan@hotmail.com>
2021-07-04 03:11:51 -0300

    menubar dinamico concluido

--- GUI/Menus/MenuBar/MenuBarButton/MenuBarButton.gd
+++ GUI/Menus/MenuBar/MenuBarButton/MenuBarButton.gd
@@ -1,11 +1,14 @@
 extends MenuButton
 
+
 var _title
 var _id: int
 
-func init(id, title):
+func init(id, title,tamanho, sub_title):
 	_id = id
 	_title = title
 	text = title
 	
+	for i in range(tamanho):
+		get_popup().add_item(String(sub_title[i]))
 
--- GUI/Menus/MenuBar/ShowMenuButtons.gd
+++ GUI/Menus/MenuBar/ShowMenuButtons.gd
@@ -1,18 +1,26 @@
 extends HBoxContainer
 
-var titles = ['File', 'Figures', 'Tools', 'Help']
+var titles = [{"name":"File", "sub_menus":["Open", "Save", "Quit"]},
+			{"name":"Figure", "sub_menus":["Triangle", "Square", "Hexagon","Vertex"]},
+			{"name":"Basic Tools", "sub_menus":["Rotation", "Scale","Translation","Shear"]},
+			{"name":"Help", "sub_menus":["About"]}]
+
 
 onready var Res = get_node("/root/MenusResolutions")
 
 var button = load("res://GUI/Menus/MenuBar/MenuBarButton/MenuBarButton.tscn")
 var buttons = []
 
+
 func _ready():
 	for i in range(titles.size()):
 		buttons.append(button.instance())
-		buttons[buttons.size() - 1].init(i, titles[i])
+		buttons[buttons.size() - 1].init(i, titles[i]["name"],titles[i]["sub_menus"].size(),titles[i]["sub_menus"])
 		add_child(buttons[buttons.size() - 1])
+	
 
 func _process(delta):
 	set_margin(MARGIN_RIGHT, Res.get_menu_bar_res().size.x)
 	set_margin(MARGIN_BOTTOM, Res.get_menu_bar_res().size.y)
+	
+
