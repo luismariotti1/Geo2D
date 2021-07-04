92b846c1a45232666eb9d67565a61df6c217f92b
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-07-04 17:23:08 -0300

    up

--- GUI/Display/Display.gd
+++ GUI/Display/Display.gd
@@ -7,38 +7,49 @@ var clicked_position = null
 var is_clicking = false
 var is_selecting = false
 var selection_tool = load("res://GUI/Display/Tools/SelectionTool/SelectionTool.tscn")
-var selection_area 
+var selection_area
 
 
 func _process(delta):
 	set_margin(MARGIN_TOP, Res.get_menu_bar_res().position.y)
 	set_margin(MARGIN_RIGHT, Res.get_display_res().size.x)
 	set_margin(MARGIN_BOTTOM, Res.get_display_res().size.y)
-	selection_area()
+	# if is_clicking == true and clicked_position != null:
+	# 	move_vertex()
+	# selection_area()
 	update()
 
+
 func is_in_display(Position):
-	if Res.get_display_res().size.y > Position.y and Position.y > Res.get_menu_bar_res().size.y and Res.get_display_res().size.x > Position.x and Position.x > Res.get_display_res().position.x: 
+	if (
+		Res.get_display_res().size.y > Position.y
+		and Position.y > Res.get_menu_bar_res().size.y
+		and Res.get_display_res().size.x > Position.x
+		and Position.x > Res.get_display_res().position.x
+	):
 		return true
 	else:
 		return false
+
 
 func _input(event):
 	if event.is_action_pressed("move_vertex"):
 		is_clicking = true
 		clicked_position = event.position
-		selection_area = selection_tool.instance()
-		selection_area.init(clicked_position)
-		add_child(selection_area)
+		move_vertex()
+		# selection_area = selection_tool.instance()
+		# selection_area.init(clicked_position)
+		# add_child(selection_area)
 	if event.is_action_released("move_vertex"):
 		is_clicking = false
 		clicked_position = null
-		selection_area.queue_free()
+		# selection_area.queue_free()
 
 
-func selection_area():
-	if is_clicking == true and clicked_position != null:
-		if is_in_display(get_global_mouse_position()):
-			selection_area.set_tool_area(get_global_mouse_position()-clicked_position)
+func move_vertex():
+	CP.mouse_position_to_cartesian(clicked_position)
 
-
+# func selection_area():
+# 	if is_clicking == true and clicked_position != null:
+# 		if is_in_display(get_global_mouse_position()):
+# 			selection_area.set_tool_area(get_global_mouse_position()-clicked_position)
--- GlobalAttributes/CartesianPlane.gd
+++ GlobalAttributes/CartesianPlane.gd
@@ -2,20 +2,31 @@ extends Node
 
 var default_x_resolution = 1024 - 256
 var divisions = 16
-var cartesian_distance = default_x_resolution/divisions
+var cartesian_distance = default_x_resolution / divisions
 var center_plan = Vector2.ZERO
+
 
 func get_cartesian_distance():
 	return cartesian_distance
 
+
 func set_center_plane(center):
 	center_plan = center
+
 
 func get_center_plane():
 	return center_plan
 
+
+func mouse_position_to_cartesian(mouse_position):
+	return ((mouse_position - center_plan) / cartesian_distance)*Vector2(1,-1)
+
 func convert_cartesian_to_pos(coord):
-	return Vector2(center_plan.x + coord.x*cartesian_distance, center_plan.y + -1*coord.y*cartesian_distance)	
+	return Vector2(
+		center_plan.x + coord.x * cartesian_distance,
+		center_plan.y + -1 * coord.y * cartesian_distance
+	)
+
 
 func convert_catersian_to_dist(coord):
-	return Vector2(coord.x*cartesian_distance,-1*coord.y*cartesian_distance)
+	return Vector2(coord.x * cartesian_distance, -1 * coord.y * cartesian_distance)
