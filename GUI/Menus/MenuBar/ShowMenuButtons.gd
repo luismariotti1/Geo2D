df08d72314baacfe2892f108c5783a659ca20437
Eb3r-r00t <eberson_taynan@hotmail.com>
2021-07-09 21:35:23 -0300

    hexagono implementado

--- GUI/Menus/MenuBar/ShowMenuButtons.gd
+++ GUI/Menus/MenuBar/ShowMenuButtons.gd
@@ -19,8 +19,9 @@ func _ready():
 		add_child(buttons[buttons.size() - 1])
 		
 
-func _process(delta):
+func _process(_delta):
 	set_margin(MARGIN_RIGHT, Res.get_menu_bar_res().size.x)
 	set_margin(MARGIN_BOTTOM, Res.get_menu_bar_res().size.y)
 	
+
 
