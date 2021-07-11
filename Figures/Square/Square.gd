c91519ca894c37800f76e915f0e3f6698b912db9
Diego <Dih.cvel@gmail.com>
2021-07-11 01:13:38 -0300

    refatoracao figure button

--- Figures/Square/Square.gd
+++ Figures/Square/Square.gd
@@ -1,17 +1,5 @@
 extends "res://Figures/Figures.gd"
 
-var new_pivot = Vector2(0, 0)
-var edge = 0
-
-
-func init(id):
-	_id = id
-	create_dic_to_properties()
-	info.insert(0, {"id": "edge", "label": "edge", "value": edge})
-	set_properties_in_inspector()
-
-func set_edge(value):
-	edge = value
 
 func _physics_process(_delta):
 	var new_vertex = []
@@ -21,17 +9,3 @@ func _physics_process(_delta):
 	new_vertex.append(CP.convert_catersian_to_dist(Vector2(edge, edge) - new_pivot))
 	new_vertex.append(CP.convert_catersian_to_dist(Vector2(edge, 0) - new_pivot))
 	vertex = new_vertex
-	if is_select:
-		update_local_variables()
-
-
-func update_local_variables():
-	edge = float(Insp.get_properties_by_id("edge"))
-
-
-func _draw():
-	draw_set_transform_matrix(t)
-	if ! filled:
-		custom_draw_polygon(vertex, color, float(line_width))
-	else:
-		custom_draw_polygon_filled(vertex, color)
