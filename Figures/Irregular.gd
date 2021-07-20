637a81f9d15ae39296803fa45e800259d152234a
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-07-20 07:52:44 -0300

    foi dificil bagarai

--- Figures/Irregular.gd
+++ Figures/Irregular.gd
@@ -2,6 +2,8 @@ extends "res://Figures/Figures.gd"
 
 var _type = "Irregular" setget , get_type
 var _is_ready = false
+var geometry = Geometry
+var new_line: PoolVector2Array
 
 func get_type():
 	return _type
@@ -22,7 +24,6 @@ func create_next_vertex(pos):
 		vertex.append(pos - inital_pos)
 		if inital_pos.distance_to(pos) <= 0.2 and vertex.size() > 2:
 			set_ready()
-	print(_is_ready)
 
 
 func _physics_process(_delta):
@@ -53,17 +54,45 @@ func convert_vertex_to_distance():
 	for i in range(vertex.size()):
 		vertex_to_draw.append(CP.convert_catersian_to_dist(vertex[i]))
 	return vertex_to_draw
-
+	
+func valid_position(line):
+	var can_draw = true
+	if vertex.size() > 1:
+		for i in range(vertex.size()-2):
+			var other_line = PoolVector2Array()
+			for j in range(2):
+				other_line.append(CP.convert_catersian_to_dist(vertex[i+j]))
+			if geometry.segment_intersects_segment_2d(other_line[0],other_line[1],line[0],line[1]) != null:
+				can_draw = false
+				break
+			other_line.resize(0)
+	return can_draw
+	
+func valid_angle(new_point):
+	var theta1 = new_point[0].angle_to_point(new_point[1])
+	var theta2 = new_point[0].angle_to_point(vertex[vertex.size()-2])
+	theta1 = rad2deg(theta1)
+	theta2 = rad2deg(theta2)
+	if abs((theta1 - theta2)) < 8:
+		return false
+	else:
+		return true
 
 func pre_render(vertex_mod):
 	var next_position = CP.mouse_position_to_cartesian(get_global_mouse_position()) - inital_pos
-	print(vertex_mod[vertex_mod.size()-1])
-	draw_line(vertex_mod[vertex_mod.size()-1], CP.convert_catersian_to_dist(next_position), color, line_width)
-
+	new_line = PoolVector2Array([vertex_mod[vertex_mod.size()-1], CP.convert_catersian_to_dist(next_position)])
+	draw_line(new_line[0], new_line[1], color, line_width)
+	draw_circle(new_line[1], 4, Color(0, 0, 0, 1))
+	if vertex.size() > 1:
+		return valid_angle(PoolVector2Array([vertex[vertex.size()-1], next_position])) and valid_position(new_line)
+	else:
+		return true
+	
 func custom_draw_polygon():
 	var vertex_mod = convert_vertex_to_distance()
 	if !_is_ready:
-		pre_render(vertex_mod)
+#		pre_render(vertex_mod)
+		print(pre_render(vertex_mod))
 	if vertex_mod.size() > 1:
 		for i in range(vertex_mod.size() - 1):
 			draw_line(vertex_mod[i], vertex_mod[i + 1], color, line_width)
