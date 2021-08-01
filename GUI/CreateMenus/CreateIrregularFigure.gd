7bc3d284f0dc920c556ffb7e59a661eef6a099f9
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-08-01 14:22:04 -0300

    up vetores dinamicos

--- GUI/CreateMenus/CreateIrregularFigure.gd
+++ GUI/CreateMenus/CreateIrregularFigure.gd
@@ -15,7 +15,11 @@ func _physics_process(delta):
 			var new_vertice = vertice.instance()
 			vertices.append(new_vertice)
 			get_node("VBoxContainer/ScrollContainer/VBoxContainer").add_child(new_vertice)
-
+			vertices[vertices.size()-1].init("v"+String(vertices.size()))
+	if num_vertices < vertices.size() and vertices.size() > 0:
+		for i in range(vertices.size()-num_vertices):
+			vertices.pop_back().queue_free()
+	
 func _on_Button_button_down():
 	pass
 
